de1b361c279859b81b2f75fc346b970809f9fd18
Johnny Trinh <johnny.trinh@hotmail.se>
2024-04-27 13:49:40 +0000

    Add PostList view, list and create a post

--- posts/serializers.py
+++ posts/serializers.py
@@ -3,11 +3,11 @@ from posts.models import Post
 
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
-    is_owner = serializers.SeralizerMethodField()
+    is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
-    def validate_image(self, value)
+    def validate_image(self, value):
         if value.size > 1024 * 1024 * 2:
             raise serializers.ValidationError(
                 'Image size larger than 2MB'
@@ -30,6 +30,6 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
-            'profile_image', 'created_on', 'updated_at',
+            'profile_image', 'created_on', 'updated_on',
             'title', 'content', 'image', 'image_filter'
         ]
