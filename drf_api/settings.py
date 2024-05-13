74a576e06a2303083e0a8d70cbb1d5cc26aee7db
Johnny Trinh <johnny.trinh@hotmail.se>
2024-05-13 04:19:40 +0000

    Add CROS ORIGINS

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -100,6 +100,10 @@ if 'CLIENT_ORIGIN_DEV' in os.environ:
         rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
+CORS_ALLOWED_ORIGINS = [
+    "https://star-review-app-fb4aac8cda63.herokuapp.com",
+]
+
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'drf_api.urls'
